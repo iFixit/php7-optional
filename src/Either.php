148218cf5e3ab610e46fcba2feb796797201f4db
Jarred Stelfox <jarstelfox@mac.com>
2019-09-25 08:34:37 -0700

    Either: Psalm type everything
    This commit gets psalm 100% passing at it's most strict settings

--- src/Either.php
+++ src/Either.php
@@ -9,6 +9,7 @@ namespace Optional;
  * @template TNone
  */
 class Either {
+   /** @var bool */
    private $hasValue;
    /** @var TSome */
    private $someValue;
@@ -30,38 +31,66 @@ class Either {
    }
 
    /**
-    * @param mixed $alternative
-    * @return mixed
+    * @param TSome $alternative
+    * @return TSome
     **/
    public function valueOr($alternative) {
       return $this->hasValue ? $this->someValue : $alternative;
    }
 
-   public function valueOrCreate(callable $func) {
-      return $this->hasValue ? $this->someValue : $func($this->noneValue);
+   /**
+    * @param callable(TNone):TSome $alternativeFactory
+    * @return TSome
+    **/
+   public function valueOrCreate(callable $alternativeFactory) {
+      return $this->hasValue ? $this->someValue : $alternativeFactory($this->noneValue);
    }
 
-   /** @param mixed $alternative */
+   /**
+    * @param TSome $alternative
+    * @return Either<TSome, TNone>
+    **/
    public function or($alternative): self {
       return $this->hasValue ? $this : self::some($alternative);
    }
 
-   public function orCreate(callable $func): self {
-      return $this->hasValue ? $this : self::some($func($this->noneValue));
+   /**
+    * @param callable(TNone):TSome $alternativeFactory
+    * @return Either<TSome, TNone>
+    **/
+   public function orCreate(callable $alternativeFactory): self {
+      return $this->hasValue ? $this : self::some($alternativeFactory($this->noneValue));
    }
 
-   public function else(self $alternativeOption): self {
-      return $this->hasValue ? $this : $alternativeOption;
+   /**
+    * @param Either<TSome, TNone> $alternativeEither
+    * @return Either<TSome, TNone>
+    **/
+   public function else(self $alternativeEither): self {
+      return $this->hasValue ? $this : $alternativeEither;
    }
 
-   public function elseCreate(callable $func): self {
-      return $this->hasValue ? $this : $func($this->noneValue);
+   /**
+    * @param callable(TNone):Either<TSome, TNone> $alternativeEitherFactory
+    * @return Either<TSome, TNone>
+    **/
+   public function elseCreate(callable $alternativeEitherFactory): self {
+      return $this->hasValue ? $this : $alternativeEitherFactory($this->noneValue);
    }
 
+   /**
+    * @template U
+    * @param callable(TSome):U $some
+    * @param callable(TNone):U $none
+    * @return U
+    **/
    public function match(callable $some, callable $none) {
       return $this->hasValue ? $some($this->someValue) : $none($this->noneValue);
    }
 
+   /**
+    * @param callable(TSome) $some
+    **/
    public function matchSome(callable $some): void {
       if (!$this->hasValue) {
          return;
@@ -70,6 +99,9 @@ class Either {
       $some($this->someValue);
    }
 
+   /**
+    * @param callable(TNone) $none
+    **/
    public function matchNone(callable $none): void {
       if ($this->hasValue) {
          return;
@@ -78,38 +110,71 @@ class Either {
       $none($this->noneValue);
    }
 
+   /**
+    * @template USome
+    * @param callable(TSome):Either<USome, TNone> $mapFunc
+    * @return Either<USome, TNone>
+    **/
    public function map(callable $mapFunc): self {
-      $someFunc = function($value) use ($mapFunc) {
+      /** @var callable(TSome):Either<USome, TNone> **/
+      $someFunc =
+      /** @param TSome $value */
+      function($value) use ($mapFunc): Either {
          return self::some($mapFunc($value));
       };
 
-      $noneFunc = function($noneValue) {
+      /** @var callable(TNone):Either<USome, TNone> **/
+      $noneFunc =
+      /** @param TNone $noneValue */
+      function($noneValue): Either {
          return self::none($noneValue);
       };
 
       return $this->match($someFunc, $noneFunc);
    }
 
+   /**
+    * @template USome
+    * @param callable(TSome):Either<USome, TNone> $mapFunc
+    * @return Either<USome, TNone>
+    **/
    public function flatMap(callable $mapFunc): self {
-      $noneFunc = function($noneValue) {
+      /** @var callable(TNone):Either<USome, TNone> **/
+      $noneFunc =
+      /** @param TNone $noneValue */
+      function($noneValue): Either {
          return self::none($noneValue);
       };
 
       return $this->match($mapFunc, $noneFunc);
    }
 
+   /**
+    * @param TNone $noneValue
+    * @return Either<TSome, TNone>
+    **/
    public function filter(bool $condition, $noneValue): self {
       return $this->hasValue && !$condition ? self::none($noneValue) : $this;
    }
 
+   /**
+    * @param callable(TSome):bool $filterFunc
+    * @param TNone $noneValue
+    * @return Either<TSome, TNone>
+    **/
    public function filterIf(callable $filterFunc, $noneValue): self {
       return $this->hasValue && !$filterFunc($this->someValue) ? self::none($noneValue) : $this;
    }
 
+   /**
+    * @param TNone $noneValue
+    * @return Either<TSome, TNone>
+    **/
    public function notNull($noneValue): self {
       return $this->hasValue && $this->someValue == null ? self::none($noneValue) : $this;
    }
 
+   /** @param mixed $value */
    public function contains($value): bool {
       if (!$this->hasValue()) {
          return false;
@@ -118,6 +183,9 @@ class Either {
       return $this->someValue == $value;
    }
 
+   /**
+    * @param callable(TSome):bool $existsFunc
+    **/
    public function exists(callable $existsFunc): bool {
       if (!$this->hasValue()) {
          return false;
@@ -126,13 +194,23 @@ class Either {
       return $existsFunc($this->someValue);
    }
 
+   /**
+    * @template U
+    * @return Option<U>
+    **/
    public function ToOption(): Option {
 
-      $someFunc = function($value) {
+      /** @var callable(TSome):Option<U> **/
+      $someFunc =
+      /** @param TSome $value **/
+      function($value): Option {
          return Option::some($value);
       };
 
-      $noneFunc = function($noneValue) {
+      /** @var callable(TNone):Option<U> **/
+      $noneFunc =
+      /** @param TSome $noneValue **/
+      function($noneValue): Option {
          return Option::none();
       };
 
@@ -143,12 +221,18 @@ class Either {
    // STATIC FACTORY FUNCTIONS //
    //////////////////////////////
 
-   /** @param TSome $someValue */
+   /**
+    * @param TSome $someValue
+    * @return Either<TSome, mixed>
+    **/
    public static function some($someValue): self {
       return new self($someValue, null, true);
    }
 
-   /** @param TNone $noneValue */
+   /**
+    * @param TNone $noneValue
+    * @return Either<mixed, TNone>
+    **/
    public static function none($noneValue): self {
       return new self(null, $noneValue, false);
    }
@@ -156,6 +240,7 @@ class Either {
    /**
     * @param TSome $someValue
     * @param TNone $noneValue
+    * @return Either<TSome, TNone>
     **/
    public static function someWhen($someValue, $noneValue, callable $filterFunc): self {
       if ($filterFunc($someValue)) {
@@ -167,6 +252,7 @@ class Either {
    /**
     * @param TSome $someValue
     * @param TNone $noneValue
+    * @return Either<TSome, TNone>
     **/
    public static function noneWhen($someValue, $noneValue, callable $filterFunc): self {
       if ($filterFunc($someValue)) {
