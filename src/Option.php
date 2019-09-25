ead41da789243d7ebd71054d66aa54489d6ba231
Daryl Metzler <daryl@ifixit.com>
2019-09-24 17:23:59 -0700

    Option: Fully type hint
    This is 100% passing on the tightest psalm settings
    Note: The psalm settings are still loose becuase Either would break.

--- src/Option.php
+++ src/Option.php
@@ -8,6 +8,7 @@ namespace Optional;
  * @template T
  */
 class Option {
+   /** @var bool */
    private $hasValue;
    /** @var T */
    private $value;
@@ -31,33 +32,58 @@ class Option {
    }
 
    /**
+    * @param callable():T $alternativeFactory
     * @return T
     **/
-   public function valueOrCreate(callable $func) {
-      return $this->hasValue ? $this->value : $func();
+   public function valueOrCreate(callable $alternativeFactory) {
+      return $this->hasValue ? $this->value : $alternativeFactory();
    }
 
-   /** @param T $alternative */
+   /**
+    * @param T $alternative
+    * @return Option<T>
+    **/
    public function or($alternative): self {
       return $this->hasValue ? $this : self::some($alternative);
    }
 
-   public function orCreate(callable $func): self {
-      return $this->hasValue ? $this : self::some($func());
+   /**
+    * @param callable():T $alternativeFactory
+    * @return Option<T>
+    **/
+   public function orCreate(callable $alternativeFactory): self {
+      return $this->hasValue ? $this : self::some($alternativeFactory());
    }
 
+   /**
+    * @param Option<T> $alternativeOption
+    * @return Option<T>
+    **/
    public function else(self $alternativeOption): self {
       return $this->hasValue ? $this : $alternativeOption;
    }
 
-   public function elseCreate(callable $func): self {
-      return $this->hasValue ? $this : $func();
+   /**
+    * @param callable():Option<T> $alternativeOptionFactory
+    * @return Option<T>
+    **/
+   public function elseCreate(callable $alternativeOptionFactory): self {
+      return $this->hasValue ? $this : $alternativeOptionFactory();
    }
 
+   /**
+    * @template U
+    * @param callable(T):U $some
+    * @param callable():U $none
+    * @return U
+    **/
    public function match(callable $some, callable $none) {
       return $this->hasValue ? $some($this->value) : $none();
    }
 
+   /**
+    * @param $some callable(T)
+    **/
    public function matchSome(callable $some): void {
       if (!$this->hasValue) {
          return;
@@ -66,6 +92,9 @@ class Option {
       $some($this->value);
    }
 
+   /**
+    * @param $none callable(T)
+    **/
    public function matchNone(callable $none): void {
       if ($this->hasValue) {
          return;
@@ -74,11 +103,20 @@ class Option {
       $none();
    }
 
+   /**
+    * @template U
+    * @param $mapFunc callable(T):U
+    * @return Option<U>
+    **/
    public function map(callable $mapFunc): self {
-      $someFunc = function($value) use ($mapFunc): self {
+      /** @var callable(T):Option<U> **/
+      $someFunc =
+      /** @param T $value **/
+      function($value) use ($mapFunc): self {
          return self::some($mapFunc($value));
       };
 
+      /** @var callable():Option<U> **/
       $noneFunc = function(): self {
          return self::none();
       };
@@ -86,7 +124,13 @@ class Option {
       return $this->match($someFunc, $noneFunc);
    }
 
+   /**
+    * @template U
+    * @param callable(T):Option<U> $mapFunc
+    * @return Option<U>
+    **/
    public function flatMap(callable $mapFunc): self {
+      /** @var callable():Option<U> **/
       $noneFunc = function(): self {
          return self::none();
       };
@@ -98,10 +142,17 @@ class Option {
       return $this->hasValue && !$condition ? self::none() : $this;
    }
 
+   /**
+    * @param callable(T):bool $filterFunc
+    * @return Option<T>
+    **/
    public function filterIf(callable $filterFunc): self {
       return $this->hasValue && !$filterFunc($this->value) ? self::none() : $this;
    }
 
+   /**
+    * @return Option<T>
+    **/
    public function notNull(): self {
       return $this->hasValue && $this->value == null ? self::none() : $this;
    }
@@ -115,6 +166,9 @@ class Option {
       return $this->value == $value;
    }
 
+   /**
+    * @param callable(T):bool $existsFunc
+    **/
    public function exists(callable $existsFunc): bool {
       if (!$this->hasValue()) {
          return false;
@@ -131,7 +185,9 @@ class Option {
     * Wraps an existing value in an Option instance.
     * Returns An optional containing the specified value.
     *
-    * @param T $someValue */
+    * @param T $someValue
+    * @return Option<T>
+    */
    public static function some($someValue): self {
       return new self($someValue, true);
    }
@@ -144,7 +200,11 @@ class Option {
       return new self(null, false);
    }
 
-   /** @param T $someValue */
+   /**
+    * @param T $someValue
+    * @param callable(T):bool $filterFunc
+    * @return Option<T>
+    **/
    public static function someWhen($someValue, callable $filterFunc): self {
       if ($filterFunc($someValue)) {
          return self::some($someValue);
@@ -152,7 +212,11 @@ class Option {
       return self::none();
    }
 
-   /** @param T $someValue */
+   /**
+    * @param T $someValue
+    * @param callable(T):bool $filterFunc
+    * @return Option<T>
+    **/
    public static function noneWhen($someValue, callable $filterFunc): self {
       if ($filterFunc($someValue)) {
          return self::none();
