9c8d1c3471b8ba1c478dccf0d455127501b26141
Jarred Stelfox <jarstelfox@mac.com>
2019-11-17 19:12:44 -0800

    Result: Add toString

--- src/Result.php
+++ src/Result.php
@@ -508,4 +508,33 @@ class Result {
       $either = Either::fromArray($array, $key, $ex);
       return new self($either);
    }
+
+   /**
+    * @psalm-suppress InvalidCast
+    *
+    * This is due to this class being a box.
+    * I can't ensure the boxed value is stringable.
+    * https://github.com/vimeo/psalm/issues/1982
+    */
+    public function __toString() {
+      $either = $this->either;
+
+      /** @var TOkay|null **/
+      $lv = $either->leftOr(null);
+
+      /** @var TError|null **/
+      $rv = $either->rightOr(null);
+
+      if ($either->isLeft()) {
+         if ($lv === null) {
+            return "Okay(null)";
+         }
+         return "Okay({$lv})";
+      } else {
+         if ($rv === null) {
+            return "Error(null)";
+         }
+         return "Error({$rv})";
+      }
+   }
 }
